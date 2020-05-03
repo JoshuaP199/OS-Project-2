6fe53053dc2a63381506f179a01e2b5fb3bb4db4
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-05-03 19:23:05 -0400

    update

---
+++ vm_sim.py
@@ -0,0 +1,11 @@
+'''
+REQUIREMENTS:
+    32-bit Logical Address
+    24-bit Physical Address
+    Page(Frame) Size: 4KB
+    Page Replacement Algorithm: LRU
+    Assuming that the page table is stored in a separated space 
+        (not in the 24-bit Physical address space)
+    Only the functional behavior is simulated. No latency 
+        simulation is required.
+'''
