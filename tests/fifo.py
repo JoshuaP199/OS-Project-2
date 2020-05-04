3d3990c0575e0c8a188e035b0fc02f3e23c161d4
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-05-04 12:35:28 -0400

    updated

--- tests/fifo.py
+++ tests/fifo.py
@@ -1,6 +1,7 @@
 #By Joshua Perez
 
 import random 
+from queue import Queue
 
 queue = []
 #numbers = [2,6,9,2,4,2,1,7,3,0,5,2,1,2,9,5,7,3,8,5] #test1
@@ -8,55 +9,91 @@ queue = []
 
 numbers = []
 
-for n in range(0,22):
+for n in range(0,100):
     numbers.append(random.randint(0, 9))
 
+capacity = int(input("Please enter the page size in KB: (dont include KB)\n"))
 
-print("numbers = ", numbers)
-print("queue = ", queue)
+#print("numbers = ", numbers)
+#print("queue = ", queue)
 print("\nStart with FIFO\n")
 
-global i, previous, pf
+global i, pf#, previous
 i = 0
-previous = False
 pf = 0
+#previous = False
 
+
+#capacity = 4 #number of pages that memory can hold
+set = []
+indexes = Queue(maxsize = capacity)
+
+#if sys.getsizeof(queue) <= 4000: #4000 is the max amount of bytes
 for num in numbers:
-    if len(queue) != 3:
-        queue.append(num)
-        print(queue)
+    if i == capacity:
+        i = 0
+    if len(set) < capacity and num not in set:
+        set.append(num)
+        print(set)
         pf += 1
-    else: 
-        if num in queue: #NEED TO CREATE FUNCTION FOR THIS SKIPPING CRAP
+        indexes.put(i)
+        i += 1
+    elif len(set) < capacity and num in set:
+        print("X")
+        continue
+    else:
+        if num in set:
             print("X")
-            if previous == True:
-                i -= 1
-            else:
-                continue
-            ''' #I think this works better ^. I think I was overthinking it
-                if i == 0: 
-                    continue
-                elif i < 2:
-                    i +=1
-                elif i == 2:
-                    continue
-                else:
-                    i = 0
-            '''
-            previous = True
-
-
-        elif i < 2:
-            queue[i] = num
-            i += 1    
-            print(queue)
-            previous = False
-            pf += 1
-
+            continue
         else:
-            queue[i] = num
-            i = 0
-            print(queue)
-            previous = False
+            #print(indexes.get())
+            set[indexes.get()] = num
+            print(set)
+            indexes.put(i)
+            #print(indexes)
+            i += 1
             pf += 1
+
+'''
+        if len(queue) != capacity:
+            queue.append(num)
+            print(queue)
+            pf += 1
+        else: 
+            if num in queue:
+                print("X")
+                if previous == True:
+                    i -= 1
+                else:
+                    continue
+'''
+'''
+                #I think this works better ^. I think I was overthinking it
+                    if i == 0: 
+                        continue
+                    elif i < 2:
+                        i +=1
+                    elif i == 2:
+                        continue
+                    else:
+                        i = 0
+'''
+'''
+                previous = True
+
+
+            elif i < (capacity-1):
+                queue[i] = num
+                i += 1    
+                print(queue)
+                previous = False
+                pf += 1
+
+            else:
+                queue[i] = num
+                i = 0
+                print(queue)
+                previous = False
+                pf += 1
+'''
 print(pf, "Page Faults")
--- tests/lru.py
+++ tests/lru.py
@@ -1,30 +1,71 @@
 #By Joshua Perez
 
 import random 
+from queue import Queue
 
 queue = []
 numbers = [2,6,9,2,4,2,1,7,3,0,5,2,1,2,9,5,7,3,8,5] #test1
 #numbers = [0,6,3,0,2,6,3,5,2,4,1,3,0,6,1,4,2,3,5,7] #test2
 '''
+numbers = []
+
 for n in range(0,22):
     numbers.append(random.randint(0, 9))
 '''
 
-print("numbers = ", numbers)
-print("queue = ", queue)
+capacity = int(input("Please enter the page size in KB: (dont include KB)\n"))
+
+#print("numbers = ", numbers)
+#print("queue = ", queue)
 print("\nStart with LRU\n")
 
 global i, pf
 i = 0
 pf = 0
 
+#capacity = 4 #number of pages that memory can hold
+set = []
+indexes = Queue(maxsize = capacity)
+
+#if sys.getsizeof(queue) <= 4000: #4000 is the max amount of bytes
+for num in numbers:
+    if i == capacity:
+        i = 0
+    if len(set) < capacity and num not in set:
+        set.append(num)
+        print(set)
+        pf += 1
+        indexes.put(i)
+        i += 1
+    elif len(set) < capacity and num in set: #how does this work with LRU since u skip to the next index in LRU?
+        print("X")
+        continue
+    else:
+        if num in set:
+            print("X")
+            rem = set.index(num)
+            #print(rem)
+            #needs to find where the dup is in set and wait till the others in set are changed before that one is
+            #indexes.get(int(rem))
+            indexes.queue.remove(rem)
+            indexes.put(rem)
+            #i = indexes.get() #needs to be head of queue without removing it
+        else:
+            #print(indexes.get())
+            set[indexes.get()] = num
+            print(set)
+            indexes.put(i)
+            #print(indexes)
+            i += 1
+
+'''
 for num in numbers:
     if len(queue) != 3:
         queue.append(num)
         print(queue)
         pf += 1
     else:
-        if num in queue: #NEED TO CREATE FUNCTION FOR THIS SKIPPING CRAP
+        if num in queue: 
             print("X")
             if num == queue[i]:
                 if i < 2:
@@ -45,4 +86,5 @@ for num in numbers:
             i = 0
             print(queue)
             pf += 1
+'''
 print(pf, "Page Faults")
